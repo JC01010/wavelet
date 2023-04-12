500464d417e81f146d5ac24fe75eb6e82fc1b155
Jason Chang <jsc008@ucsd.edu>
2023-04-12 16:03:42 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -23,21 +23,20 @@ class Handler implements URLHandler {
                 }
             }
         } 
-        else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/search")) {
-                List <String> list2 = new ArrayList<String>();
-                String retS = "";
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    for (int i = 0; i < list.size(); i++) {
-                        if (list.get(i).indexOf(parameters[1])>=0) {
-                            list2.add(list.get(i));
-                            retS = retS + list.get(i) + "\n";
-                        }
-                    return retS;
-                    }
-                }
+        else if (url.getPath().contains("/search")) {
+                return "asdf";
+                // List <String> list2 = new ArrayList<String>();
+                // String retS = "DFJKLSKL";
+                // String[] parameters = url.getQuery().split("=");
+                // if (parameters[0].equals("s")) {
+                //     for (int i = 0; i < list.size(); i++) {
+                //         if (list.get(i).indexOf(parameters[1])>=0) {
+                //             list2.add(list.get(i));
+                //             retS = retS + list.get(i) + "\n";
+                //         }
+                //     return retS;
+                //     }
+                // }
             }
 
             
